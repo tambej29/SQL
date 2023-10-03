1168c2525f9e4f946b93972781ac8425dc8d4f4b
Jordan Tambeck <68528130+tambej29@users.noreply.github.com>
2023-10-03 08:44:49 -0700

    Update foodie-fi Script.sql

--- Case 3 Foodie-Fi/foodie-fi Script.sql
+++ Case 3 Foodie-Fi/foodie-fi Script.sql
@@ -141,7 +141,7 @@ WHERE b.plan_id= 3 AND a.plan_id = 0;
 
 -- 10. Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)    
 SELECT
-	month,
+	time,
     	count(customer_id) as num_cust,
 	ROUND(AVG(DATEDIFF(annual_start, trial_start))) avg_days
 FROM
@@ -162,7 +162,7 @@ FROM
 			 WHEN datediff(b.start_date, a.start_date) <= 300 THEN '10 months'
 			 WHEN datediff(b.start_date, a.start_date) <= 330 THEN '11 months'
 			 WHEN datediff(b.start_date, a.start_date) <= 360 THEN '1 year'
-			 ELSE '1 + year' END AS month
+			 ELSE '1 + year' END AS time
 	FROM subscriptions AS a
 	JOIN subscriptions AS b USING(customer_id)
 	WHERE b.plan_id= 3 AND a.plan_id = 0
